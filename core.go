78c70a214ef5f5975ebae4023773e4a06199d969
Валерий Голдун <goldun.valeriy@wb.ru>
2024-07-17 11:18:10 +0300

    хелсчеки и оптимизация

--- core.go
+++ core.go
@@ -1,6 +1,7 @@
 package fxprovider
 
 import (
+	"github.com/ValGoldun/clerk"
 	"github.com/ValGoldun/fxprovider/appcontext"
 	"github.com/ValGoldun/fxprovider/config"
 	fxlogger "github.com/ValGoldun/fxprovider/logger"
@@ -9,19 +10,19 @@ import (
 	"go.uber.org/fx"
 )
 
-func ProvideApplicationCore[Config any](lifecycle fx.Lifecycle) (*appcontext.AppContext, logger.Logger, Config, error) {
+func ProvideApplicationCore[Config any](lifecycle fx.Lifecycle) (*appcontext.AppContext, logger.Logger, Config, clerk.Clerk, error) {
 	ctx := appcontext.New()
 	var cfg Config
 
 	cfg, err := config.New[Config](ctx)
 	if err != nil {
-		return nil, logger.Logger{}, cfg, err
+		return nil, logger.Logger{}, cfg, clerk.Clerk{}, err
 	}
 
 	logger, err := fxlogger.New(ctx, lifecycle)
 	if err != nil {
-		return nil, logger, cfg, err
+		return nil, logger, cfg, clerk.Clerk{}, err
 	}
 
-	return ctx, logger, cfg, nil
+	return ctx, logger, cfg, clerk.New(logger), nil
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.2
 
 require (
 	github.com/ValGoldun/bsonregistry v1.0.0
+	github.com/ValGoldun/clerk v1.0.0
 	github.com/ValGoldun/logger v1.0.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/penglongli/gin-metrics v0.1.10
@@ -24,6 +25,7 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
