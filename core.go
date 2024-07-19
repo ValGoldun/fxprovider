0b749787f7385ccb5f7859d97bcfe7cc30756704
Валерий Голдун <goldun.valeriy@wb.ru>
2024-07-19 23:36:16 +0300

    хелсчеки и оптимизация

--- core.go
+++ core.go
@@ -1,7 +1,6 @@
 package fxprovider
 
 import (
-	"github.com/ValGoldun/clerk"
 	"github.com/ValGoldun/fxprovider/appcontext"
 	"github.com/ValGoldun/fxprovider/config"
 	fxlogger "github.com/ValGoldun/fxprovider/logger"
@@ -10,19 +9,19 @@ import (
 	"go.uber.org/fx"
 )
 
-func ProvideApplicationCore[Config any](lifecycle fx.Lifecycle) (*appcontext.AppContext, logger.Logger, Config, clerk.Clerk, error) {
+func ProvideApplicationCore[Config any](lifecycle fx.Lifecycle) (*appcontext.AppContext, logger.Logger, Config, error) {
 	ctx := appcontext.New()
 	var cfg Config
 
 	cfg, err := config.New[Config](ctx)
 	if err != nil {
-		return nil, logger.Logger{}, cfg, clerk.Clerk{}, err
+		return nil, logger.Logger{}, cfg, err
 	}
 
 	logger, err := fxlogger.New(ctx, lifecycle)
 	if err != nil {
-		return nil, logger, cfg, clerk.Clerk{}, err
+		return nil, logger, cfg, err
 	}
 
-	return ctx, logger, cfg, clerk.New(logger), nil
+	return ctx, logger, cfg, nil
 }
